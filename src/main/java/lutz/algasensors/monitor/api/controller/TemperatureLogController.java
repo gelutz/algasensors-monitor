adcad49a246b4a74d29a9f82aed0fda8bc4b4637
gelutz <gabriel.lutz5@gmail.com>
2025-11-02 14:25:01 -0300

    feat: criando controller de log de temperatura

---
+++ src/main/java/lutz/algasensors/monitor/api/controller/TemperatureLogController.java
@@ -0,0 +1,34 @@
+package lutz.algasensors.monitor.api.controller;
+
+import io.hypersistence.tsid.TSID;
+import lombok.RequiredArgsConstructor;
+import lutz.algasensors.monitor.api.model.TemperatureLogOutput;
+import lutz.algasensors.monitor.domain.model.SensorId;
+import lutz.algasensors.monitor.domain.repository.TemperatureLogRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/sensors/{sensorId}/temperatures")
+@RequiredArgsConstructor
+public class TemperatureLogController {
+	private final TemperatureLogRepository temperatureLogRepository;
+
+	@GetMapping
+	public Page<TemperatureLogOutput> search(@PathVariable TSID sensorId, @PageableDefault Pageable pageable) {
+
+		return temperatureLogRepository
+				.findAllBySensorId(new SensorId(sensorId), pageable)
+				.map(logs ->
+						new TemperatureLogOutput(
+								logs.getId(),
+								logs.getSensorId().getValue(),
+								logs.getRegisteredAt(),
+								logs.getTemperature()));
+	}
+}
